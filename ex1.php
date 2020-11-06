0d75ee3b1dfa3cd21bcda8ff6cca42a3ee4b50f8
wanatchaporn9339 <73693820+wanatchaporn9339@users.noreply.github.com>
2020-11-06 18:16:15 +0700

    Add files via upload

---
+++ ex1.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex1 layout</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row row-height">
+            <div class="col-md-6 cell">
+                <div class="cell-body">
+                    <img src="bkk1.jpg" alt="" width="100%">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row height-100">
+                    <div class="col-sm-6">
+                        <div class="row height-100">
+                            <div class="col-12 cell" style="height: 33%;">
+                                <div class="cell-body">
+                                    <img src="แฟดำ.jpg" alt="" width="100%">
+                                </div>
+                            </div>
+                            <div class="col-12 cell" style="height: 33%;">
+                                <div class="cell-body">
+                                    <img src="old town.jpg" alt="" width="100%">
+                                </div>
+                            </div>
+                            <div class="col-12 cell" style="height: 33%;">
+                                <div class="cell-body">
+                                    <img src="rusia.jpg" alt="" width="100%">
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="row height-100">
+                            <div class="col-12 cell" style="height: 66%;">
+                                <div class="cell-body">
+                                    <img src="korea2.jpg" alt="" width="100%" height="100%">
+                                </div>
+                            </div>
+                            <div class="col-12 cell" style="height: 34%;">
+                                <div class="cell-body">
+                                    <img src="jap1.jpg" alt="" width="100%">
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
